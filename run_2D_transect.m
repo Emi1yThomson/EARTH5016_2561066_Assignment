4126d245a64438b989aeef467593c770e4e0a282
Emily <emilyt5150@gmail.com>
2024-12-03 12:32:23 +0000

    Updated variables

--- run_2D_transect.m
+++ run_2D_transect.m
@@ -14,19 +14,20 @@ n_units = 9;        % number of rock units contained in image
 % Nz = target no. of rows in z-direction
 [units,D,Nz] = ModelFromImage('section.tiff',n_units,W,Nx);
 
-% material properties for each rock unit (update based on your calibration)
+% material properties for each rock unit taken from provided excel file and
+% DOI: 10.1023/B:NARR.0000032647.41046.e7.
 
 matprop = [
 % unit  conductivity  density  heat capacity  heat production
-   1	    3.6788	    2697.6	    1000	    4.172
-   2	    1	        2000	    1000	    1
-   3	    3.2197	    2703.5	    1000	    5.575
-   4	    1	        2000	    1000	    1
-   5	    1	        2000	    1000	    1
-   6	    0.924	    2083.1	    1000	    1
-   7	    1	        2000	    1000	    1
-   8	    0.919	    1905.9	    1000	    1
-   9	    1e-6        1000	    1000	    0];  % air/water
+  1	    3.6788	    2697.6	    600 	    4.172           % Granite phase 1, alternative Cp 1172
+  2	    1	        2700	    770	        1               % Basement gneiss, alternative Cp 979
+  3	    3.2197	    2703.5	    600	        5.575           % Granite phase 2
+  4	    1	        1942.3	    740	        1               % Sand
+  5	    1	        2648	    740	        1               % Gravel
+  6	    0.924	    2081.7	    860	        1               % Clay, mudstone
+  7	    1	        1916	    910	        1               % Silt
+  8	    0.919	    1909.78	    740	        1               % Mud, silt, sand
+  9	    1e-6        1000	    1000	    0];             % air/water
 
 % get coefficient fields based on spatial distribution of rock units from image
 
@@ -39,12 +40,13 @@ Hr     = reshape(matprop(units,5),Nz,Nx); % heat rate
 k0 = kT ./ (rho .* Cp);
 
 % set model parameters
-
-
 dTdz = [0, 35/1000];  % set boundary condition
-T0  = 0;              % surface temperature
+T0  = 5;              % surface temperature degree C
+Tair = 5;             % air temperature degree C
 nop   = 100;          % output figure produced every 'nop' steps
 yr    = 3600*24*365;  % seconds per year [s]
-tend  = 1e4*yr;       % stopping time [s]
+tend  = 1e7*yr;       % stopping time [s]
+CFL   = 1/5;         % Time step limiter
+
 %*****  RUN MODEL
 run('./transect_2D.m');
